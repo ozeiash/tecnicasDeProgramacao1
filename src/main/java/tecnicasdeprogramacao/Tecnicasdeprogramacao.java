1117f2883ab556d9f583145ad38f2e0858ce6ddc
Ozeias <ozeiash@gmail.com>
2024-03-02 17:28:27 -0300

    Meu primeiro versionamento.

---
+++ src/main/java/tecnicasdeprogramacao/Tecnicasdeprogramacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tecnicasdeprogramacao;
+
+/**
+ *
+ * @author ozeias
+ */
+public class Tecnicasdeprogramacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
